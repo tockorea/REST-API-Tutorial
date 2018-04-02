7a6edefdcd4af2e5c2365b2699873921972a0d70
tockorea <lim.chae.eun@gmail.com>
2018-04-03 06:59:24 +0900

    Creating Routes

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const express = require('express');
 // set up express app
 const app = express();
 
-app.get('/', function(req, res) {
-  console.log('GET Request');
-  res.send({ name: 'Yoshi' });
-})
+// initialize routes
+app.use('/api', require('./routes/api'));
 
 // listen for requests
 app.listen(process.env.port || 4000, function() {
