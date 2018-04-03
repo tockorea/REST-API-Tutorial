b169c61da41d30c6921d5b7cd2d7046e1e1c2bee
tockorea <lim.chae.eun@gmail.com>
2018-04-04 00:19:39 +0900

    Handling MongoDB

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(req, res) {
-  res.send({ type: 'GET' });
+router.get('/ninjas', function(req, res, next) {
+  Ninja.aggregate([{
+    $geoNear: {
+      near: {
+        type: 'Point',
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      spherical: true,
+      distanceField: 'dist',
+      maxDistance: 100000
+    }
+  }]).then(function(ninjas) {
+    res.send(ninjas);
+  }).catch(next);
 });
 
 // add a new ninja to the db
-router.post('/ninjas', function(req, res) {
-  console.log(req.body);
-  res.send({ type: 'POST', name: req.body.name, rank: req.body.rank });
+router.post('/ninjas', function(req, res, next) {
+  Ninja.create(req.body).then(function(ninja) {
+    res.send(ninja);
+  }).catch(next);
 });
 
 // update a ninja in the db
-router.put('/ninjas/:id', function(req, res) {
-  res.send({ type: 'PUT' });
+router.put('/ninjas/:id', function(req, res, next) {
+  Ninja.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
+    Ninja.findOne({ _id: req.params.id }).then(function(ninja) {
+      res.send(ninja);
+    }).catch(next)
+  })
 });
 
 // delete a ninjas from the db
-router.delete('/ninjas/:id', function(req, res) {
-  res.send({ type: 'DELETE' });
+router.delete('/ninjas/:id', function(req, res, next) {
+  Ninja.findByIdAndRemove({ _id: req.params.id }).then(function(ninja) {
+    res.send(ninja);
+  }).catch(next)
 });
 
 module.exports = router;
