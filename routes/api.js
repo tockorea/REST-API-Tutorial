ed6a049e5ef77012b25803b3c61f7efcff9a41a3
tockorea <lim.chae.eun@gmail.com>
2018-04-03 20:52:23 +0900

    Handling POST Requests

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,8 @@ router.get('/ninjas', function(req, res) {
 
 // add a new ninja to the db
 router.post('/ninjas', function(req, res) {
-  res.send({ type: 'POST' });
+  console.log(req.body);
+  res.send({ type: 'POST', name: req.body.name, rank: req.body.rank });
 });
 
 // update a ninja in the db
